2fd39cb0a4cba0e3263e97450480aab342e9c703
Vicente Jimenez <vic20git@gmail.com>
2019-05-13 14:30:07 +0200

    Add files via upload

---
+++ Scripts/Anaconda_Python_Console.bat
@@ -0,0 +1,7 @@
+SET PATH=%PATH%;\Anaconda_Win64
+SET PATH=%PATH%;\Anaconda_Win64\Scripts
+SET PATH=%PATH%;\Anaconda_Win64\Library\bin; 
+SET PYTHONPATH=\SLab\Code
+cd \SLab\Console
+python.exe 
+
---
+++ Scripts/Anaconda_Scripts_CMD.bat
@@ -0,0 +1,6 @@
+SET PATH=%PATH%;\Anaconda_Win64
+SET PATH=%PATH%;\Anaconda_Win64\Scripts
+SET PATH=%PATH%;\Anaconda_Win64\Library\bin;
+SET PYTHONPATH=\SLab\Code
+cd \Anaconda_Win64\Scripts
+start cmd /K
